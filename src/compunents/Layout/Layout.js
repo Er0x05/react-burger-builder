6072022aa531ab3821e69cf93c7e691aa3586049
Pek <pek.devit@gmail.com>
2020-07-21 18:01:39 +0800

    fix css

--- src/compunents/Layout/Layout.js
+++ src/compunents/Layout/Layout.js
@@ -4,14 +4,31 @@ import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <React.Fragment>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </React.Fragment>
-);
+class layout extends React.Component {
+    state = {
+        switchSdrawerDrop: false
+    }
+
+    toSwitchSdrawerDrop = () => {
+        this.setState((prevState)=>{
+            return { switchSdrawerDrop: !prevState.switchSdrawerDrop }
+        })
+    }
+    
+    render(){
+        return(
+            <React.Fragment>
+                <Toolbar
+                    clicked={this.toSwitchSdrawerDrop} />
+                <SideDrawer
+                    show={this.state.switchSdrawerDrop}
+                    close={this.toSwitchSdrawerDrop} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </React.Fragment>   
+        )
+    }
+};
 
 export default layout;
--- src/compunents/Navigation/SideDrawer/SideDrawer.js
+++ src/compunents/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import BackDrop from '../../UI/Backdrop/Backdrop';
 import styles from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
 
+    let attachedCss = [ styles.SideDrawer, styles.Close ];
+    props.show && ( attachedCss = [ styles.SideDrawer, styles.Open ] );
+
     return (
-        <div className={styles.SideDrawer}>
-            <div className={styles.Logo}>
-                <Logo />
-            </div>
-            <nav>
-                <NavItems />
-            </nav>
-        </div>   
+        <React.Fragment>
+            <BackDrop show={props.show} closeModal={props.close} />
+            <div className={attachedCss.join(' ')}>
+                <div className={styles.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavItems />
+                </nav>
+            </div>   
+        </React.Fragment>
     )
 };
 
